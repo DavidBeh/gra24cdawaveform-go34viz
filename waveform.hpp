22a6f8214dea46f9ce92cb72b134aca434fdf233
go34viz <go34viz@mytum.de>
2024-05-29 11:31:21 +0200

    modulea

--- waveform.hpp
+++ waveform.hpp
@@ -2,6 +2,7 @@
 #define H_WAVEFORM_HPP
 
 #include <systemc>
+
 using namespace sc_core;
 
 // TODO: Implement the modules MODULE_A and MODULE_B so that their `out` values follow
@@ -10,17 +11,37 @@ using namespace sc_core;
 //       You may add additional signals and/or variables if necessary.
 
 SC_MODULE(MODULE_A) {
-  sc_out<bool> out;
-  // TODO: Implement module A.
+    sc_out<bool> out;
+    sc_in<bool> clk;
+    bool value;
+    int counter;
+    int stretch;
+    SC_CTOR(MODULE_A) {
+    }
+
+    MODULE_A(sc_module_name, int stretch) : value(false), stretch(stretch) {
+        SC_CTHREAD(behavior, clk);
+    }
+
+    void behavior() {
+        while (true) {
+            wait();
+            if (counter++ % stretch == 0) {
+                value != value;
+                out.write(value);
+            }
+        }
+    }
+    // TODO: Implement module A.
 };
 
 SC_MODULE(MODULE_B) {
-  MODULE_A a1;
-  MODULE_A a2;
+    MODULE_A a1;
+    MODULE_A a2;
 
-  sc_out<bool> out;
+    sc_out<bool> out;
 
-  // TODO: Implement module B.
+    // TODO: Implement module B.
 };
 
 #endif
